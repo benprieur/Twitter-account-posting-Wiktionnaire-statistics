e4555d5abc135d3372a3875bd44485c2ddc37394
Benoît Prieur <agamitsudo@gmail.com>
2019-09-01 04:44:40 +0200

    xxx

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
 import pywikibot
 from pywikibot import pagegenerators
 import tweepy
+import datetime
+import time
 
 def GetWiktionaryData():
-
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
+    print (api.me().name)
     siteWiktionnaire = pywikibot.Site('fr', u'wiktionary')
-
     elapse = True
-
     while True:
         try:
-
             if elapse == True:
 
                 str_end = siteWiktionnaire.getcurrenttimestamp()
                 end = int(str_end)
+                print(end)
 
-                start = end - 120
+                start = end - 300
+                print(start)
 
                 for page in pagegenerators.RecentChangesPageGenerator(end, start):
+                    print(page.title())
 
-                    if str(page.title).find(":") == -1:
+                    ''' main '''
+                    if str(page.title()).find(":") == -1:
 
+                        ''' Créations '''
                         try:
                             if (page.exists() == True):
-
-                                print(page.title(), "AVANT TEST")
                                 timestamprev = page.oldest_revision.timestamp
                                 rev = str(timestamprev.year)
                                 if int(timestamprev.month) < 10:
@@ -45,25 +47,27 @@ def GetWiktionaryData():
                                 rev = rev + str(timestamprev.minute)
                                 if int(timestamprev.second) < 10:
                                     rev = rev + "0"
-
                                 rev = rev + str(timestamprev.second)
-                                irev = int(rev)
 
-                                if irev >= start and len(rev) == 14:
-                                    MESSAGE = "[[ " + page.title() + " ]] "
-                                    api.update_status(MESSAGE)
-
+                                print (str(rev))
+                                print(str(start))
+                                if int(rev) > start:
+                                    titre = str(page.title())
+                                    url = str(page.full_url())
+                                    msg = "[[ " + titre + " ]] " + url + " "
+                                    print(msg)
+                                    api.update_status(msg)
                         except ValueError:
                             print ("Oops!")
 
             str_mnt = siteWiktionnaire.getcurrenttimestamp()
-            MAINTENANT = int(str_mnt) - 120
+            mnt = int(str_mnt) - 300
+            print(mnt)
 
-            if MAINTENANT >= end:
+            if mnt > end:
                 elapse = True
             else:
                 elapse = False
-
         except ValueError:
             print("Oops!")
 
